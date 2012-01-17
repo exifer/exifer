b18ccd2f2fcffbabf94a8399fa62ca6e1382eeb8
Igor Golovanov <igor.golovanov@gmail.com>
2012-01-17 22:53:46 +0200

    Common classes and interfaces.

---
+++ lib/Exifer/Common/MetadataWriterInterface.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Exifer package.
+ *
+ * (c) Igor Golovanov <igor.golovanov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Exifer\Common;
+
+/**
+ * @author  Igor Golovanov <igor.golovanov@gmail.com>
+ */
+interface MetadataWriterInterface
+{
+    
+}
