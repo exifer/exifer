fd520e196f5ef857b8528234390fb51ad81b0969
Igor Golovanov <igor.golovanov@gmail.com>
2012-01-17 23:17:53 +0200

    Iptc skeleton of classes and interfaces.

---
+++ lib/Exifer/Iptc/Tags/City.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the Exifer package.
+ *
+ * (c) Igor Golovanov <igor.golovanov@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Exifer\Iptc;
+
+use Exifer\Iptc\IptcTag;
+
+/**
+ * @author  Igor Golovanov <igor.golovanov@gmail.com>
+ */
+class City extends IptcTag
+{
+    
+}
